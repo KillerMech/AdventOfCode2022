12157da7be7179fe7fb5ccd368cb419bd98110db
Matthew <sephy7killermech@gmail.com>
2023-09-22 09:13:39 -0400

    Day_4 is borked

--- src/day_4.rs
+++ src/day_4.rs
@@ -3,10 +3,47 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 pub fn day_4() {
-    // File hosts.txt must exist in the current path
+    // File assignments.txt must exist in the current path
+    let mut final_count: u32 = 0;
+    
     if let Ok(lines) = read_lines("./assignments.txt") {
-        let first_team: u32;2;
-        let second_team: u32;2;
+        for line in lines {
+            let fully_contained: bool = get_pair_fully_contained(&line.unwrap());
+        }
+    }
+}
+
+fn get_pair_fully_contained(unformatted_str: &str) -> bool {
+    let fully_contained: bool;
+    let mut first_assignment = [0,0];
+    let mut second_assignment = [0,0];
+
+    for character in unformatted_str.chars() {
+        let mut team_switch: bool = false;
+        let mut assign_switch: bool = false;
+
+        if character == ',' {
+            team_switch = true;
+        }
+        if character == '-' {
+            assign_switch = true;
+        }
+        if team_switch == false {
+            if assign_switch == false {
+                first_assignment[0] = character.parse.unwrap();
+            }
+            else {
+                first_assignment[1] = character.parse.unwrap();
+            }
+        }
+        else {
+            if assign_switch == false {
+                second_assignment[0] = character.parse.unwrap();
+            }
+            else {
+                second_assignment[1] = character.parse.unwrap();
+            }
+        }
     }
 }
 
